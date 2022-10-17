bd9a8784e0e39567f7088e79b1022f42e0f8f802
Emile <emile.vianneyliaud@gmail.com>
2022-10-17 15:47:01 +0200

    set project and task Schema

--- models/project.js
+++ models/project.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
-// Define the schema for users
+// Define the schema for projects
 const projectSchema = new Schema({
-  name: String
+  name: String,
+  active: Boolean,
+  description: String,
+  company: String
 });
 // Create the model from the schema and export it
-export default mongoose.model('User', projectSchema);
+export default mongoose.model('Project', projectSchema);
--- models/task.js
+++ models/task.js
@@ -1,8 +1,26 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-// Define the schema for users
-const taskSchema = new Schema({
-  name: String
-});
-// Create the model from the schema and export it
-export default mongoose.model('User', taskSchema);
+  import mongoose from 'mongoose';
+  const Schema = mongoose.Schema;
+  // Define the schema for the tasks
+  const taskSchema = new Schema({
+    name: String,
+    startDate: { type: Date, default: Date.now  },
+    endDate: { type: Date, default: Date.now  },
+    duration: Number,
+    description: {
+      type: String, // Type validation
+      maxlength: 200 // Maximum length
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Project'
+    }
+  });
+
+  taskSchema.index({ startDate: 1, name: 1 });
+
+  // Create the model from the schema and export it: Task = tasks in the DB
+  export default mongoose.model('Task', taskSchema);
